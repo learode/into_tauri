73623b3eaa83b7fea9d3a2d3ede42deecb558177
mulfranck <njifranclin@gmail.com>
2023-02-06 02:21:11 +0100

    listening on windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(all(not(debug_assertions), target_os="windows"), windows_subsystem = "windows")]
-use tauri::{Menu, MenuItem, Submenu, CustomMenuItem };
+use tauri::{Menu, CustomMenuItem};
+use tauri::{WindowBuilder};
 
 
 
@@ -10,52 +11,34 @@ fn greet(name: &str) -> String {
 }
 
 
+
 fn main() {
-    // Menu items instances
-    // define menu for file related action
-    let quit = CustomMenuItem::new("quit", "Quit");
-    let close = CustomMenuItem::new("close", "Close");
-
-    let file_submenu = Submenu::new("File", Menu::new().add_item(quit)
-                        .add_item(close));
-    
-    // Menu for Help, with sub being about, release note and video tutorial
-    let release_note_mi = CustomMenuItem::new("release_note", "Release Notes");
-    let video_tutorial_mi = CustomMenuItem::new("video_tutorials", "Video Tutorials");
-    let about_mi = CustomMenuItem::new("about", "About Us");
-
-    let help_submenu = Submenu::new("Help", Menu::new()
-                            .add_item(release_note_mi)
-                            .add_item(video_tutorial_mi)
-                            .add_item(about_mi)
-                        );
-
-    let menu = Menu::new()
-        .add_native_item(MenuItem::Copy)
-        // add menu context
-        // .add_item(CustomMenuItem::new("hide", "Hide"))
-        // Add a menu with its content as defined
-        .add_submenu(file_submenu)
-        .add_submenu(help_submenu);
-
-
-
-    tauri::Builder::default()
-        .menu(menu)
-        .on_menu_event(|event| {
-            match event.menu_item_id() {
-                "quit" => {
-                    println!("{:?}", event);
-                    std::process::exit(0);
-                },
-                "close" => event.window().close().unwrap(),
-                // "release_note" => app.exit(0),
-                // "video_tutorials" => app.exit(0),
-                // "about" => app.exit(0),
-                _ => {}
-            }
-        })
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("Error while initialising tauri application");
+  let menu = Menu::new().add_item(CustomMenuItem::new("finish", "Finish")); // configure the menu
+  
+  tauri::Builder::default()
+    .setup(|app| {
+      let window = WindowBuilder::new(
+        app,
+        "main-window".to_string(),
+        tauri::WindowUrl::App("index.html".into()),
+      )
+      .menu(menu)
+      .build()?;
+      let window_ = window.clone();
+      window.on_menu_event(move |event| {
+        match event.menu_item_id() {
+          "quit" => {
+            std::process::exit(0);
+          }
+          "close" => {
+            window_.close().unwrap();
+          }
+          _ => {}
+        }
+      });
+      Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
