b6f77bd4e28bfcc3705badb2fb54c800cd276e95
mulfranck <njifranclin@gmail.com>
2023-02-17 02:36:10 +0100

    menu entry and progess tracking folder instead of commits

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,38 +1,42 @@
 #![cfg_attr(all(not(debug_assertions), target_os="windows"), windows_subsystem = "windows")]
-use tauri::{Menu, MenuItem, CustomMenuItem, AboutMetadata};
+use tauri::{Menu, MenuItem, CustomMenuItem, Builder, MenuEntry, Submenu, Manager};
 
 /**
- * Trying to understand the use of MenuItem.
+ * Trying to understand the use of Menu, Menu::with_items, MenuEntry.
  * Its implemented as using the native menu items but I donot understand why its not working.
  * 
  * Only about is unsupported
  */
 
+ #[tauri::command]
+ fn greet(name: &str) -> String {
+    format!("Yo nigga {name}, whats up?")
+ }
+
 
 fn main() {
-    let menu = Menu::new().
-                add_native_item(MenuItem::About(
-                    // The name passed is appended to the about mkx it ugly :ha:ha:
-                    // Without the name, the about has no app name :fustrating:
-                    String::from("Liferoute"), 
-                    tauri::AboutMetadata::default()
-                        .authors(vec![String::from("Mulfranck"), "Tauri".to_string()])
-                        .version("0.1.0")
-                        .license("MIT")
-                        .comments("Knowing Tauri, is a test environment to learn and get a feel of using Tauri for desktop app\n :ha:ha:")
-                        .website("https://github.com/learode/liferoutine")
-                ))
-                .add_native_item(MenuItem::Copy) // Only About is implimemted/supported
-                .add_item(CustomMenuItem::new("quit", "Quit"));
-
-    tauri::Builder::default()
-        .menu(menu)
+    Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .menu(Menu::with_items([
+            MenuEntry::Submenu(Submenu::new(
+                "File", 
+                Menu::with_items([
+                    CustomMenuItem::new("new", "New").into(),
+                    CustomMenuItem::new("open", "Open").into(),
+                    CustomMenuItem::new("more", "More").into(),
+                ])
+            ))
+        ]))
         .on_menu_event(|event| {
             match event.menu_item_id() {
-                "quit" => { std::process::exit(0); },
-                _ => { println!("What? What did you click nigga?"); }
+                "new" => { println!("Still to learn how :sobbing:") },
+                "more" => { 
+                    tauri::api::shell::open(&event.window().shell_scope(), "https://github.com/learode/liferoute", None).unwrap()
+                },
+                _ => { println!("Jez Rust is a pain, I just forgot this statement You have to complain niga?");  }
             }
         })
         .run(tauri::generate_context!())
-        .expect("Failed to build app");
+        .expect("Wow! Erro while trying to build a window :ha:");
+    ()
 }
